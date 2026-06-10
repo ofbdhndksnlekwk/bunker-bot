862a9b7c3d13f0112dc1f997eb2f13521c788e11
ofbdhndksnlekwk <daromadyoutube77@gmail.com>
2026-06-10 11:48:14 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
 BOT_TOKEN = "8710879281:AAFdUUoDYBzUAX1LgeX9Bzn-W9PdPpOslHU"
 PG_URL = "postgresql://neondb_owner:npg_s4xIBX1TDZMm@ep-orange-credit-aq9sshqz-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-REDIS_URL = "redis://localhost:6379"
